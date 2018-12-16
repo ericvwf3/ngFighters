a73e1877ed2485b4df0de256937786647b8641bf
Thomas Sihapanya <sihapanya.thomas@gmail.com>
2018-12-16 20:45:59 +0100

    :sparkles: Finally, implementing Combat feature!

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -60,6 +60,28 @@ export class HeroService {
             defense: 6,
             experience: 0,
             level: 0
+        },
+
+        {
+            name: 'Aquaman',
+            in_battle: true,
+            life: 1,
+            max_life: 1,
+            strengh: 1,
+            defense: 1,
+            experience: 0,
+            level: 0
+        },
+
+        {
+            name: 'Hulk',
+            in_battle: true,
+            life: 100,
+            max_life: 100,
+            strengh: 100,
+            defense: 100,
+            experience: 0,
+            level: 0
         }
     ];
 
@@ -85,13 +107,9 @@ export class HeroService {
             return false;
         } else {
             // Sinon, on combat
-
-            /**
-             * Combat
-             */
+            this.heroes[index].in_battle = true;
 
             // On génère un monstre selon le niveau du héros
-
             const monster = this.monsterService.generate(this.heroes[index].level);
 
             // tslint:disable-next-line:max-line-length
@@ -110,8 +128,10 @@ export class HeroService {
 
     rest(index: number) {
 
-        // Si le héros a suffisamment d'expérience pour se repose
+        // Si le héros a suffisamment d'expérience pour se reposer
         if (this.heroes[index].experience >= this.restCost) {
+
+            this.heroes[index].in_battle = false;
 
             // Le héros ne peut se reposer que si sa vie est inférieure à son max_life
             if (this.heroes[index].life < this.heroes[index].max_life) {
@@ -136,7 +156,43 @@ export class HeroService {
         }
     }
 
-    fight(index: number, monster: Object) {
+    fight(index: number, monster) {
+
+        // L'attaque réelle est l'attaque du personnage moins la défense de l'autre.
+        let monsterRealAttack = monster.strengh - this.heroes[index].defense;
+        let heroRealAttack = this.heroes[index].strengh - monster.defense;
+
+        // Si l'attaque est moins forte que la défense, alors elle est égale à 0.
+        if (monsterRealAttack < 0) {
+            monsterRealAttack = 0;
+        }
+
+        if (heroRealAttack < 0) {
+            heroRealAttack = 0;
+        }
+
+
+        // On combat tant que l'un ou l'autre n'est pas KO
+        while (this.heroes[index].life > 0 && monster.life > 0) {
+
+            alert('PV de ' + this.heroes[index].name + ': ' + this.heroes[index].life + '\nPV du Monstre: ' + monster.life);
+
+            this.heroes[index].life = this.heroes[index].life - monsterRealAttack;
+            monster.life = monster.life - heroRealAttack;
+
+        }
+
+        // Cas où le héros est KO
+        if (this.heroes[index].life <= 0) {
+
+            this.heroes[index].life = 0;
+            alert('Fin du match: ' + this.heroes[index].name + ' est tombé KO...');
+        }
+
+        // Cas où le héros est KO
+        if (monster.life <= 0) {
+            alert('Fin du match: BRAVO ! Le monstre est tombé KO !');
+        }
 
     }
 
